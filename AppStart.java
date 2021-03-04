26522310d23e9756f33eeb74e04eb20bf45b0449
Pedro <200221135@estudantes.ips.pt>
2021-03-04 16:59:35 +0000

    New AppStart

---
+++ AppStart.java
@@ -0,0 +1,23 @@
+
+/**
+ * Escreva a descrição da classe AppStart aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+public class AppStart{
+   public static void main(String[] args) {
+       int ordem=1;
+       Size Medium = Size.MEDIUM;
+       
+       System.out.println("Name          : " + Medium.toString());
+       System.out.println("tostring()    : " + Medium.getDescription());
+       System.out.println("Ordem         : " + ordem);
+       System.out.println("Valor Minimo  : " + Medium.getMinValue());
+       System.out.println("Valor Maximo  : " + Medium.getMaxValue());
+       System.out.println("Codigo        : " + Medium.getCode());
+    }
+       
+       
+    }
+    
